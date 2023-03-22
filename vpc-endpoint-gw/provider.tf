93c3b00db07459d36e66dcfbb4d332becd486cc2
Ashok Chakravarthi <seeashok89@gmail.com>
2023-03-22 13:10:27 +0800

    Endpoint

---
+++ vpc-endpoint-gw/provider.tf
@@ -0,0 +1,17 @@
+# -- provider.tf (Provider) -- #
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = "xxxx"
+  secret_key = "bo+xxxx+xxx"
+}
